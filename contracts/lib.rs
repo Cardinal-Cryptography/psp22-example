325a7829a942cac93c4af86b1500f5b18d6f72c5
deuszx <mateusz.gorski@cardinals.cc>
2022-11-16 19:41:37 +0100

    Emit events as per PSP22 spec.

--- contracts/lib.rs
+++ contracts/lib.rs
@@ -4,12 +4,19 @@
 
 #[openbrush::contract]
 mod psp22_example {
+    use ink_lang::{
+        codegen::{EmitEvent, Env},
+        reflect::ContractEventBase,
+    };
     use ink_prelude::string::String;
     use ink_storage::traits::SpreadAllocate;
     use openbrush::{
         contracts::psp22::{self, extensions::metadata, psp22::Internal},
         traits::Storage,
     };
+
+    /// Event type
+    pub type Event = <Psp22Example as ContractEventBase>::Type;
 
     #[ink(storage)]
     #[derive(Default, SpreadAllocate, Storage)]
@@ -43,9 +50,68 @@ mod psp22_example {
                 instance.metadata.decimals = decimals;
             })
         }
+
+        // Emit event abstraction. Otherwise ink! deserializes events incorrectly when there are events from more than one contract.
+        pub fn emit_event<EE: EmitEvent<Self>>(emitter: EE, event: Event) {
+            emitter.emit_event(event);
+        }
     }
 
     impl psp22::PSP22 for Psp22Example {}
 
     impl metadata::PSP22Metadata for Psp22Example {}
+
+    // Overwrite the `psp22::Internal` trait to emit the events as described in the PSP22 spec:
+    // https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#transfer
+    impl psp22::Internal for Psp22Example {
+        fn _emit_transfer_event(
+            &self,
+            _from: Option<AccountId>,
+            _to: Option<AccountId>,
+            _amount: Balance,
+        ) {
+            Psp22Example::emit_event(
+                self.env(),
+                Event::Transfer(Transfer {
+                    from: _from,
+                    to: _to,
+                    value: _amount,
+                }),
+            )
+        }
+
+        fn _emit_approval_event(&self, _owner: AccountId, _spender: AccountId, _amount: Balance) {
+            Psp22Example::emit_event(
+                self.env(),
+                Event::Approval(Approval {
+                    owner: _owner,
+                    spender: _spender,
+                    value: _amount,
+                })
+            )
+        }
+    }
+
+    /// Event emitted when a token transfer occurs.
+    #[ink(event)]
+    #[derive(Debug)]
+    pub struct Transfer {
+        #[ink(topic)]
+        pub from: Option<AccountId>,
+        #[ink(topic)]
+        pub to: Option<AccountId>,
+        pub value: Balance,
+    }
+
+    /// Event emitted when an approval occurs that `spender` is allowed to withdraw
+    /// up to the amount of `value` tokens from `owner`.
+    #[ink(event)]
+    #[derive(Debug)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
 }
